dd093d81b876045175f8958f20721ebd28b6d77a
Nikita <astraimer.ru@gmail.com>
2024-03-14 20:59:21 +0100

    working on the sword

--- src/main/java/nikita/uniquescythe/items/custom/WardenersSwordItem.java
+++ src/main/java/nikita/uniquescythe/items/custom/WardenersSwordItem.java
@@ -1,11 +1,20 @@
 package nikita.uniquescythe.items.custom;
 
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 
 
 public class WardenersSwordItem extends AxeItem {
+	public static int cooldown;
 	public WardenersSwordItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
 		super(material, attackDamage, attackSpeed, settings);
 	}
@@ -15,5 +24,14 @@ public class WardenersSwordItem extends AxeItem {
 		return ActionResult.FAIL;
 	}
 
+	public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+		target.addStatusEffect(new StatusEffectInstance(StatusEffects.DARKNESS, 100, 1));
+		target.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 100, 1));
 
+
+
+		return super.postHit(stack, target, attacker);
+	}
+
+	//to do something if right-clicked too
 }
