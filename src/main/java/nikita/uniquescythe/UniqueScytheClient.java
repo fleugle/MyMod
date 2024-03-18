329151a66ea66a50ff23992c911fb0721386bb95
Nikit <astraimer.ru@gmail.com>
2024-03-19 00:11:23 +0100

    something in rendering this f entity, but still far away from waht i want

--- src/main/java/nikita/uniquescythe/UniqueScytheClient.java
+++ src/main/java/nikita/uniquescythe/UniqueScytheClient.java
@@ -9,6 +9,7 @@ import net.minecraft.client.render.entity.model.EntityModelLayer;
 import nikita.uniquescythe.entities.ModEntities;
 import nikita.uniquescythe.entities.client.ModModelLayers;
 import nikita.uniquescythe.entities.client.WindChargeProjectileEntityModel;
+import nikita.uniquescythe.entities.client.WindChargeProjectileEntityRenderer;
 import nikita.uniquescythe.particles.ModParticles;
 import nikita.uniquescythe.particles.custom.VoidAttackParticle;
 
@@ -18,9 +19,15 @@ public class UniqueScytheClient implements ClientModInitializer {
 		//wtf is that
 		ParticleFactoryRegistry.getInstance().register(ModParticles.VOID_ATTACK_PARTICLE, VoidAttackParticle.Factory::new);
 
-		EntityRendererRegistry.register(ModEntities.WIND_CHARGE_PROJECTILE, FlyingItemEntityRenderer::new); //FlyingItemEntityRenderer::new needs to be changed in  advance
+		//EntityRendererRegistry.register(ModEntities.WIND_CHARGE_PROJECTILE, FlyingItemEntityRenderer::new); //FlyingItemEntityRenderer::new needs to be changed in  advance
+
+		EntityRendererRegistry.register(ModEntities.WIND_CHARGE_PROJECTILE, WindChargeProjectileEntityRenderer::new); //FlyingItemEntityRenderer::new needs to be changed in  advance
 
 
 		EntityModelLayerRegistry.registerModelLayer(ModModelLayers.WIND_CHARGE, WindChargeProjectileEntityModel::getTexturedModelData);
+
+
+
+
 	}
 }
--- src/main/java/nikita/uniquescythe/entities/client/WindChargeProjectileEntityRenderer.java
+++ src/main/java/nikita/uniquescythe/entities/client/WindChargeProjectileEntityRenderer.java
@@ -1,11 +1,36 @@
 package nikita.uniquescythe.entities.client;
 
-import net.minecraft.client.render.block.entity.MobSpawnerBlockEntityRenderer;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Axis;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.render.OverlayTexture;
 import nikita.uniquescythe.entities.custom.WindChargeProjectileEntity;
 
-public class WindChargeProjectileEntityRenderer {
+public class WindChargeProjectileEntityRenderer extends FlyingItemEntityRenderer<WindChargeProjectileEntity> {
 
+	public WindChargeProjectileEntityRenderer(EntityRendererFactory.Context context) {
+		super(context);
+	}
+	@Override
+	public void render(WindChargeProjectileEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+		matrices.push();
+		matrices.scale(1, 1, 1);
+		matrices.multiply(this.dispatcher.getRotation());
+		matrices.multiply(Axis.Y_POSITIVE.rotationDegrees(180.0F));
+
+		// Get the model data for your WindChargeProjectileEntityModel
+		WindChargeProjectileEntityModel<WindChargeProjectileEntity> model = new WindChargeProjectileEntityModel<>(WindChargeProjectileEntityModel.getTexturedModelData().createModel());
+
+		// Render the model
+		model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(this.getTexture(entity))), light, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
+
+		matrices.pop();
+	}
 }
