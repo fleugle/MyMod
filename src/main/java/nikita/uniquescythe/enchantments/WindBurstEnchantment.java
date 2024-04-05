f0c3dbf3b02451700445cf4d4bef971454404ba2
Nikit <astraimer.ru@gmail.com>
2024-04-06 01:31:11 +0200

    reviewed the part with enchantment itself, so that it targets mace

--- src/main/java/nikita/uniquescythe/enchantments/WindBurstEnchantment.java
+++ src/main/java/nikita/uniquescythe/enchantments/WindBurstEnchantment.java
@@ -1,5 +1,6 @@
 package nikita.uniquescythe.enchantments;
 
+import com.chocohead.mm.api.ClassTinkerers;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
@@ -7,6 +8,8 @@ import net.minecraft.entity.EquipmentSlot;
 public class WindBurstEnchantment extends Enchantment {
 
 	protected WindBurstEnchantment() {
-		super(Rarity.UNCOMMON, EnchantmentTarget.WEAPON,new EquipmentSlot[] {EquipmentSlot.MAINHAND});
+		super(Rarity.UNCOMMON,
+			ClassTinkerers.getEnum(EnchantmentTarget.class, "MACE")
+			,new EquipmentSlot[] {EquipmentSlot.MAINHAND});
 	}
 }
