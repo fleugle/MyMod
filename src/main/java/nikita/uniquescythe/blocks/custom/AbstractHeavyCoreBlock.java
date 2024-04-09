e0d582ac053548bbdf5fe74d99eec9c6d889a183
Nikita <astraimer.ru@gmail.com>
2024-04-09 22:14:12 +0200

    now good👌

--- src/main/java/nikita/uniquescythe/blocks/custom/AbstractHeavyCoreBlock.java
+++ src/main/java/nikita/uniquescythe/blocks/custom/AbstractHeavyCoreBlock.java
@@ -2,6 +2,7 @@ package nikita.uniquescythe.blocks.custom;
 
 import net.minecraft.block.*;
 import net.minecraft.block.enums.JigsawOrientation;
+import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
@@ -9,6 +10,8 @@ import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
@@ -26,11 +29,23 @@ public class AbstractHeavyCoreBlock extends Block implements Waterloggable {
 	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 	public AbstractHeavyCoreBlock( Settings settings) {
 		super(settings);
-		this.setDefaultState(this.stateManager.getDefaultState().with(WATERLOGGED, Boolean.valueOf(false)));
+		this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()
+			.with(WATERLOGGED, false))
+			.with(ORIENTATION, JigsawOrientation.NORTH_UP));
+
 	}
 
 
 
+	@Override
+	public BlockState rotate(BlockState state, BlockRotation rotation) {
+		return (BlockState)state.with(ORIENTATION, rotation.getDirectionTransformation().mapJigsawOrientation(state.get(ORIENTATION)));
+	}
+
+	@Override
+	public BlockState mirror(BlockState state, BlockMirror mirror) {
+		return (BlockState)state.with(ORIENTATION, mirror.getDirectionTransformation().mapJigsawOrientation(state.get(ORIENTATION)));
+	}
 
 
 	@Override
@@ -45,8 +60,24 @@ public class AbstractHeavyCoreBlock extends Block implements Waterloggable {
 	public BlockState getPlacementState(ItemPlacementContext ctx) {
 		FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
 		boolean bl = fluidState.getFluid() == Fluids.WATER;
-		return super.getPlacementState(ctx).with(WATERLOGGED, Boolean.valueOf(bl));
+
+		Direction direction = ctx.getSide();
+		Direction direction2 = direction.getAxis() == Direction.Axis.Y ? ctx.getPlayerFacing().getOpposite() : Direction.UP;
+		//return super.getPlacementState(ctx).with(WATERLOGGED, Boolean.valueOf(bl));
+		return ((BlockState)this.getDefaultState()
+			.with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).isOf(Fluids.WATER))
+			.with(ORIENTATION, JigsawOrientation.byDirections(direction, direction2)));
 	}
+
+
+
+	//thing to make a shape.
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+		return OUTLINE_SHAPE;
+	}
+
+
 
 	@Override
 	public BlockState getStateForNeighborUpdate(
@@ -66,14 +97,11 @@ public class AbstractHeavyCoreBlock extends Block implements Waterloggable {
 
 	@Override
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(ORIENTATION);
 		builder.add(WATERLOGGED);
 	}
 
 
 
-	//thing to make a shape.
-	@Override
-	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-		return OUTLINE_SHAPE;
-	}
+
 }
