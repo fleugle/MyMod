21da863b3b4d0e086c8073381a0b657604d8b4fc
Nikit <astraimer.ru@gmail.com>
2024-04-11 00:10:53 +0200

    mixin has been done, now it is time for some refinement. Like more cool particles, effects and, more importantly - my next goal is to do a cool effect with the mace using aaaparticle library. I mean, why else would I add it here??

---
+++ src/main/java/nikita/uniquescythe/mixin/ModifySweepingParticle.java
@@ -0,0 +1,31 @@
+package nikita.uniquescythe.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleEffect;
+import nikita.uniquescythe.items.ModItems;
+import nikita.uniquescythe.particles.ModParticleTypes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(PlayerEntity.class)
+public abstract class ModifySweepingParticle {
+	@ModifyArg(
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/server/world/ServerWorld;spawnParticles(Lnet/minecraft/particle/ParticleEffect;DDDIDDDD)I",
+			ordinal = 0
+		),
+		method = "spawnSweepAttackParticles"
+	)
+	private <T extends ParticleEffect> T changeSweepAttackParticle(T particle) {//idk how to name it properly for now
+		PlayerEntity player = (PlayerEntity)(Object)this;
+		//i guess this if statements do the work here. to be experimented with in advance
+		if (player.getMainHandStack().isOf(ModItems.FROSTY_SCYTHE)) return (T) ModParticleTypes.FROSTY_SWEEP_ATTACK;
+
+
+
+
+		return particle;
+	}
+}
