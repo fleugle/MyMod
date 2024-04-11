d03d05a4aee6d809b38b1b3af6f8f767faaae270
Nikita <astraimer.ru@gmail.com>
2024-04-11 21:40:25 +0200

    sdg\\sdg\ds

--- src/main/java/nikita/uniquescythe/mixin/ModifyCritParticle.java
+++ src/main/java/nikita/uniquescythe/mixin/ModifyCritParticle.java
@@ -1,33 +1,17 @@
 package nikita.uniquescythe.mixin;
 
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.particle.ParticleEffect;
-import nikita.uniquescythe.items.ModItems;
-import nikita.uniquescythe.particles.ModParticleTypes;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.particle.ParticleTypes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(PlayerEntity.class)
+@Mixin(targets = "net.minecraft.client.render.block.BlockModelRenderer$AmbientOcclusionCalculator")//change
 public abstract class ModifyCritParticle {
-	@ModifyArg(
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/client/network/ClientPlayerEntity;addEmitter(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/core/particles/ParticleOptions;)I",
-			ordinal = 0
-		),
-		method = "addCritParticles"
-	)
-	private <T extends ParticleEffect> T changeSweepAttackParticle(T particle) {//idk how to name it properly for now
-		PlayerEntity player = (PlayerEntity)(Object)this;
-		//i guess this if statements do the work here. to be experimented with in advance
-		if (player.getMainHandStack().isOf(ModItems.FROSTY_SCYTHE)) return (T) ModParticleTypes.FROSTY_SWEEP_ATTACK;
-
-
-		if (player.getMainHandStack().isOf(ModItems.WANDERERS_SWORD)) return (T) ModParticleTypes.VOID_SWEEP_ATTACK;
 
 
 
-		return particle;
-	}
+
 }
