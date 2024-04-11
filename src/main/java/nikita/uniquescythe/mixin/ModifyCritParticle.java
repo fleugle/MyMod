96f8df0f446f24a7537ba72200d9a9220d20c0fa
Nikit <astraimer.ru@gmail.com>
2024-04-11 19:12:59 +0200

    👍🤠🔫 --> 💀

---
+++ src/main/java/nikita/uniquescythe/mixin/ModifyCritParticle.java
@@ -0,0 +1,33 @@
+package nikita.uniquescythe.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleEffect;
+import nikita.uniquescythe.items.ModItems;
+import nikita.uniquescythe.particles.ModParticleTypes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(PlayerEntity.class)
+public abstract class ModifyCritParticle {
+	@ModifyArg(
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/network/ClientPlayerEntity;addEmitter(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/core/particles/ParticleOptions;)I",
+			ordinal = 0
+		),
+		method = "addCritParticles"
+	)
+	private <T extends ParticleEffect> T changeSweepAttackParticle(T particle) {//idk how to name it properly for now
+		PlayerEntity player = (PlayerEntity)(Object)this;
+		//i guess this if statements do the work here. to be experimented with in advance
+		if (player.getMainHandStack().isOf(ModItems.FROSTY_SCYTHE)) return (T) ModParticleTypes.FROSTY_SWEEP_ATTACK;
+
+
+		if (player.getMainHandStack().isOf(ModItems.WANDERERS_SWORD)) return (T) ModParticleTypes.VOID_SWEEP_ATTACK;
+
+
+
+		return particle;
+	}
+}
