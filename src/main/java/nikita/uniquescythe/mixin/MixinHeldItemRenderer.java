effe1f1b681a5b39ddfa433ce22d70f8aa47e54c
Nikita <astraimer.ru@gmail.com>
2024-04-14 21:26:58 +0200

    to change arm pos

---
+++ src/main/java/nikita/uniquescythe/mixin/MixinHeldItemRenderer.java
@@ -0,0 +1,11 @@
+package nikita.uniquescythe.mixin;
+
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.render.item.HeldItemRenderer;
+
+
+
+@Mixin(HeldItemRenderer.class)
+public class MixinHeldItemRenderer {
+}
