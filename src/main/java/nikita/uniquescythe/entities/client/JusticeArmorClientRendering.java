1c320f0253f96219bbced6d3731a053ff9a86e3b
Nikita <astraimer.ru@gmail.com>
2024-04-19 21:36:16 +0200

    model but needs resolve

---
+++ src/main/java/nikita/uniquescythe/entities/client/JusticeArmorClientRendering.java
@@ -0,0 +1,33 @@
+package nikita.uniquescythe.entities.client;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import nikita.uniquescythe.UniqueScythe;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.quiltmc.qsl.rendering.entity.api.client.ArmorRenderingRegistry;
+
+public class JusticeArmorClientRendering implements ArmorRenderingRegistry.TextureProvider{
+
+
+	private static final Identifier ARMOR_TEXTURE_ID = UniqueScythe.id("textures/models/armor/JusticeArmor.png");
+
+
+	@Override
+
+
+
+	@Override
+	public @NotNull Identifier getArmorTexture(@NotNull Identifier texture, @NotNull LivingEntity entity,
+											   @NotNull ItemStack stack, @NotNull EquipmentSlot slot, boolean useSecondLayer, @Nullable String suffix)
+	{
+		if (slot == EquipmentSlot.HEAD) {
+			// redirect texture
+			return ARMOR_TEXTURE_ID;
+		}
+
+		return texture;
+	}
+}
---
+++ src/main/java/nikita/uniquescythe/entities/client/JusticeArmorModel.java
@@ -0,0 +1,52 @@
+package nikita.uniquescythe.entities.client;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.*;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+
+public class JusticeArmorModel extends BipedEntityModel<LivingEntity> {
+	private final ModelPart helmet;
+	private final ModelPart chestplate;
+	private final ModelPart rarm;
+	private final ModelPart larm;
+	private final ModelPart rleg;
+	private final ModelPart lleg;
+	public JusticeArmorModel(ModelPart root) {
+        this.helmet = root.getChild("helmet");
+		this.chestplate = root.getChild("chestplate");
+		this.rarm = root.getChild("rarm");
+		this.larm = root.getChild("larm");
+		this.rleg = root.getChild("rleg");
+		this.lleg = root.getChild("lleg");
+	}
+	public static TexturedModelData getTexturedModelData() {
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
+		ModelPartData helmet = modelPartData.addChild("helmet", ModelPartBuilder.create().uv(0, 0).cuboid(-7.0F, 0.8125F, -7.0F, 14.0F, 1.0F, 14.0F, new Dilation(0.0F))
+		.uv(0, 15).cuboid(-4.5F, -3.6875F, -4.5F, 9.0F, 4.5F, 9.0F, new Dilation(0.0F))
+		.uv(56, 1).cuboid(-5.0F, -3.1875F, -5.0F, 10.0F, 4.0F, 10.0F, new Dilation(0.0F))
+		.uv(0, 0).cuboid(-7.0F, 0.8125F, -7.0F, 14.0F, 1.0F, 14.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -6.0625F, 0.0F, -0.1309F, -0.0002F, 0.0854F));
+
+		ModelPartData chestplate = modelPartData.addChild("chestplate", ModelPartBuilder.create().uv(36, 15).cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.25F))
+		.uv(0, 55).cuboid(-3.0F, -0.5F, -3.0F, 6.0F, 3.0F, 6.0F, new Dilation(0.25F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+		ModelPartData rarm = modelPartData.addChild("rarm", ModelPartBuilder.create().uv(1, 29).cuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.pivot(-5.0F, 2.0F, 0.0F));
+
+		ModelPartData larm = modelPartData.addChild("larm", ModelPartBuilder.create().uv(17, 29).cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.pivot(5.0F, 2.0F, 0.0F));
+
+		ModelPartData rleg = modelPartData.addChild("rleg", ModelPartBuilder.create(), ModelTransform.pivot(-1.9F, 12.0F, 0.0F));
+
+		ModelPartData lleg = modelPartData.addChild("lleg", ModelPartBuilder.create(), ModelTransform.pivot(1.9F, 12.0F, 0.0F));
+		return TexturedModelData.of(modelData, 128, 64);
+	}
+
+
+	@Override
+	public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
+
+	}
+}
