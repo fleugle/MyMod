aec124f076a0a2e06d570663388341a616619a5f
Nikita <astraimer.ru@gmail.com>
2024-04-20 13:44:10 +0200

    💀 🔫🥸

--- src/main/java/nikita/uniquescythe/UniqueScytheClient.java
+++ src/main/java/nikita/uniquescythe/UniqueScytheClient.java
@@ -42,7 +42,11 @@ public class UniqueScytheClient implements ClientModInitializer {
 		//mod model layers
 		EntityModelLayerRegistry.registerModelLayer(ModModelLayers.WIND_CHARGE, WindChargeProjectileEntityModel::getTexturedModelData);//wind charge
 
+		EntityModelLayerRegistry.registerModelLayer(ModModelLayers.JUSTICE_ARMOR, JusticeArmorModel::getTexturedModelData);//wind charge
+
 		EntityModelLayerRegistry.registerModelLayer(ModModelLayers.BREEZE, BreezeEntityModel::getTexturedModelData);//breeze
+
+
 
 		EntityRendererRegistry.register(ModEntities.WIND_CHARGE_PROJECTILE, WindChargeProjectileEntityRenderer::new);
 
