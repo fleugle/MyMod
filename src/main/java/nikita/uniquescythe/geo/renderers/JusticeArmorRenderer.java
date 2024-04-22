e1e66e9f0152b94cd4bb8bf3277ee4f4b3b40589
Nikit <astraimer.ru@gmail.com>
2024-04-22 20:39:40 +0200

    crashes. I'll try to fix it this afternoon

--- src/main/java/nikita/uniquescythe/geo/renderers/JusticeArmorRenderer.java
+++ src/main/java/nikita/uniquescythe/geo/renderers/JusticeArmorRenderer.java
@@ -3,9 +3,7 @@ package nikita.uniquescythe.geo.renderers;
 import net.minecraft.util.Identifier;
 import nikita.uniquescythe.UniqueScythe;
 import nikita.uniquescythe.items.custom.JusticeArmorItem;
-import software.bernie.geckolib.GeckoLib;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
-import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
 public final class JusticeArmorRenderer extends GeoArmorRenderer<JusticeArmorItem> {
