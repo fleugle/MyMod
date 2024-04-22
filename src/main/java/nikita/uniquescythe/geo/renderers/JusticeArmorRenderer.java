b170ac03babbb1442f51c13558f7090efff901e4
Nikita <astraimer.ru@gmail.com>
2024-04-22 19:09:16 +0200

    works now

--- src/main/java/nikita/uniquescythe/geo/renderers/JusticeArmorRenderer.java
+++ src/main/java/nikita/uniquescythe/geo/renderers/JusticeArmorRenderer.java
@@ -2,14 +2,14 @@ package nikita.uniquescythe.geo.renderers;
 
 import net.minecraft.util.Identifier;
 import nikita.uniquescythe.UniqueScythe;
-import nikita.uniquescythe.geo.models.JusticeArmorModel;
 import nikita.uniquescythe.items.custom.JusticeArmorItem;
+import software.bernie.geckolib.GeckoLib;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
 public final class JusticeArmorRenderer extends GeoArmorRenderer<JusticeArmorItem> {
-	public JusticeArmorRenderer(GeoModel<JusticeArmorItem> model) {
-		super(new JusticeArmorModel());
+	public JusticeArmorRenderer() {
+		super(new DefaultedItemGeoModel<>(new Identifier(UniqueScythe.MOD_ID, "armor/justice_armor")));
 	}
 }
--- src/main/java/nikita/uniquescythe/items/custom/JusticeArmorItem.java
+++ src/main/java/nikita/uniquescythe/items/custom/JusticeArmorItem.java
@@ -1,20 +1,31 @@
 package nikita.uniquescythe.items.custom;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import nikita.uniquescythe.geo.models.JusticeArmorModel;
 import nikita.uniquescythe.geo.renderers.JusticeArmorRenderer;
+import nikita.uniquescythe.items.ModItems;
+import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.animatable.client.RenderProvider;
+import software.bernie.geckolib.constant.DataTickets;
+import software.bernie.geckolib.constant.DefaultAnimations;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -22,24 +33,19 @@ public class JusticeArmorItem extends ArmorItem implements GeoItem {
 	private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 	private final Supplier<Object> renderProvider = GeoItem.makeRenderer(this);
 
-
-
-	public JusticeArmorItem(ArmorMaterial material, ArmorSlot slot, Settings settings) {
-		super(material, slot, settings);
+	public JusticeArmorItem(ArmorMaterial armorMaterial, ArmorItem.ArmorSlot type, Settings properties) {
+		super(armorMaterial, type, properties);
 	}
-
 
 	@Override
 	public void createRenderer(Consumer<Object> consumer) {
 		consumer.accept(new RenderProvider() {
-
-			JusticeArmorModel model;
 			private GeoArmorRenderer<?> renderer;
 
 			@Override
-			public BipedEntityModel<LivingEntity> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, BipedEntityModel<LivingEntity> original) {
-				if(this.renderer == null)
-					this.renderer = new JusticeArmorRenderer(model);
+			public @NotNull BipedEntityModel<LivingEntity> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, BipedEntityModel<LivingEntity> original) {
+				if (this.renderer == null)
+					this.renderer = new JusticeArmorRenderer();
 
 				// This prepares our GeoArmorRenderer for the current render frame.
 				// These parameters may be null however, so we don't do anything further with them
@@ -55,13 +61,46 @@ public class JusticeArmorItem extends ArmorItem implements GeoItem {
 		return this.renderProvider;
 	}
 
+	// Let's add our animation controller
 	@Override
-	public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
+	public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+		controllers.add(new AnimationController<>(this, 20, state -> {
+			// Apply our generic idle animation.
+			// Whether it plays or not is decided down below.
+			state.getController().setAnimation(DefaultAnimations.IDLE);
 
+			// Let's gather some data from the state to use below
+			// This is the entity that is currently wearing/holding the item
+			Entity entity = state.getData(DataTickets.ENTITY);
+
+			// We'll just have ArmorStands always animate, so we can return here
+			if (entity instanceof ArmorStandEntity)
+				return PlayState.CONTINUE;
+
+			// For this example, we only want the animation to play if the entity is wearing all pieces of the armor
+			// Let's collect the armor pieces the entity is currently wearing
+			Set<Item> wornArmor = new ObjectOpenHashSet<>();
+
+			for (ItemStack stack : entity.getArmorItems()) {
+				// We can stop immediately if any of the slots are empty
+				if (stack.isEmpty())
+					return PlayState.STOP;
+
+				wornArmor.add(stack.getItem());
+			}
+
+			// Check each of the pieces match our set
+			boolean isFullSet = wornArmor.containsAll(ObjectArrayList.of(
+				ModItems.JUSTICE_HAT,
+				ModItems.COWBOYS_CLOAK));
+
+			// Play the animation if the full set is being worn, otherwise stop
+			return isFullSet ? PlayState.CONTINUE : PlayState.STOP;
+		}));
 	}
 
 	@Override
 	public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return this.cache;
+		return this.cache;
 	}
 }
