db1c03e6728e4373c2abc5c3f1736fcc96ba3cc8
Nikita <astraimer.ru@gmail.com>
2024-04-25 20:55:03 +0200

    dcfh

---
+++ src/main/java/nikita/uniquescythe/utility/GuiltyLevelSystem.java
@@ -0,0 +1,17 @@
+package nikita.uniquescythe.utility;
+
+import net.minecraft.entity.player.PlayerEntity;
+
+public class GuiltyLevelSystem {
+
+	public static void addGuiltyLevelsToPlayer(PlayerEntity player, int amount){
+		CommandsExecuter.executeCommand(player, "");
+
+	}
+
+	public static void JoinScoreboard(PlayerEntity player){
+		CommandsExecuter.executeCommand(player, "");
+
+	}
+
+}
