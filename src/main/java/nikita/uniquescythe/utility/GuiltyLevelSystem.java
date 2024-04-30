a302ccb6eff8f9cc3c828081c38aac9cb8fb6b94
Nikita <astraimer.ru@gmail.com>
2024-04-30 23:48:38 +0200

    some work done, especially with build in textures

--- src/main/java/nikita/uniquescythe/utility/GuiltyLevelSystem.java
+++ src/main/java/nikita/uniquescythe/utility/GuiltyLevelSystem.java
@@ -6,6 +6,7 @@ import net.minecraft.scoreboard.ScoreboardCriterion;
 import net.minecraft.scoreboard.ScoreboardObjective;
 import net.minecraft.text.Text;
 import nikita.uniquescythe.UniqueScythe;
+import org.jetbrains.annotations.NotNull;
 
 public class GuiltyLevelSystem {
 
@@ -14,7 +15,7 @@ public class GuiltyLevelSystem {
 
 	}
 
-	public static void createIntScoreboadIfMissing(Scoreboard scoreboard, ScoreboardCriterion criteria, String objectiveName){
+	public static void createIntScoreboadIfMissing(@NotNull Scoreboard scoreboard, ScoreboardCriterion criteria, String objectiveName){
 
 		ScoreboardObjective objective = scoreboard.getObjective(objectiveName);
 
@@ -24,9 +25,9 @@ public class GuiltyLevelSystem {
 
 			//additional check
 			if (scoreboard.getObjective(objectiveName) != null){
-				UniqueScythe.LOGGER.info("objective "+ objectiveName +" with criteria "+ criteria +" exists");
+				UniqueScythe.LOGGER.info("objective "+ objectiveName +" with criteria "+ criteria +" has been created");
 			}else UniqueScythe.LOGGER.info("failed objective "+ objectiveName + " with criteria " + criteria + " creation");
-		}
+		}else UniqueScythe.LOGGER.info(objectiveName + " objective "+ " with criteria " + criteria +"already exists");
 	}
 
 
