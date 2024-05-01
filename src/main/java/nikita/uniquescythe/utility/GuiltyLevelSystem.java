30efe4d53fa76bf84568f686f47fab80d4ca0bb6
Nikita <astraimer.ru@gmail.com>
2024-05-01 22:00:13 +0200

    some work done.
    Technically - texturepack (it's main part) is finished(still, maybe some changes will be made).
    
    Also I'm nearly finished with the guilty system, but fasr away from its implementation.🥸

--- src/main/java/nikita/uniquescythe/utility/GuiltyLevelSystem.java
+++ src/main/java/nikita/uniquescythe/utility/GuiltyLevelSystem.java
@@ -4,14 +4,18 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.ScoreboardCriterion;
 import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.scoreboard.ScoreboardPlayerScore;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import nikita.uniquescythe.UniqueScythe;
 import org.jetbrains.annotations.NotNull;
 
 public class GuiltyLevelSystem {
 
-	public static void addGuiltyLevelsToPlayer(PlayerEntity player, int amount){
-		CommandsExecuter.executeCommand(player, "");
+	public static void addGuiltyLevelsToPlayer(PlayerEntity player, String playerName, int amount){
+
+
+		CommandsExecuter.executeCommand(player, "scoreboard players add "+ playerName +" PersistentGuiltyLevel "+ amount);
 
 	}
 
@@ -28,6 +32,24 @@ public class GuiltyLevelSystem {
 				UniqueScythe.LOGGER.info("objective "+ objectiveName +" with criteria "+ criteria +" has been created");
 			}else UniqueScythe.LOGGER.info("failed objective "+ objectiveName + " with criteria " + criteria + " creation");
 		}else UniqueScythe.LOGGER.info(objectiveName + " objective "+ " with criteria " + criteria +"already exists");
+	}
+
+	public static int getGuiltyLevel(ServerPlayerEntity player, String playerName, String objectiveName){
+
+		Scoreboard scoreboard = player.getWorld().getScoreboard();
+		ScoreboardObjective objective = scoreboard.getObjective(objectiveName);
+		scoreboard.getPlayerScore(playerName, objective);
+
+
+		ScoreboardPlayerScore score = scoreboard.getPlayerScore(playerName, objective);
+
+
+		return score.getScore();
+	}
+
+	public static Scoreboard getScoreboard(PlayerEntity player){
+
+		return player.getWorld().getScoreboard();
 	}
 
 
