9dfb733bb4f9cacf8d0e346d79091c53b12504dc
Nikit <astraimer.ru@gmail.com>
2024-05-02 23:31:11 +0200

    bug fixed

--- src/main/java/nikita/uniquescythe/items/custom/InnocentNimbusItem.java
+++ src/main/java/nikita/uniquescythe/items/custom/InnocentNimbusItem.java
@@ -1,40 +1,28 @@
 package nikita.uniquescythe.items.custom;
 
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import mod.azure.azurelib.animatable.GeoItem;
 import mod.azure.azurelib.animatable.client.RenderProvider;
-import mod.azure.azurelib.constant.DataTickets;
-import mod.azure.azurelib.constant.DefaultAnimations;
 import mod.azure.azurelib.core.animatable.instance.AnimatableInstanceCache;
 import mod.azure.azurelib.core.animation.AnimatableManager;
 import mod.azure.azurelib.core.animation.AnimationController;
 import mod.azure.azurelib.core.animation.RawAnimation;
-import mod.azure.azurelib.core.object.PlayState;
 import mod.azure.azurelib.renderer.GeoArmorRenderer;
 import mod.azure.azurelib.util.AzureLibUtil;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import nikita.uniquescythe.geo.renderers.InnocentNimbusRenderer;
-import nikita.uniquescythe.geo.renderers.JusticeArmorRenderer;
-import nikita.uniquescythe.items.ModItems;
 import nikita.uniquescythe.utility.ModArmorMaterials;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.include.com.google.common.collect.ImmutableMap;
 
-import java.util.Map;
-import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -42,6 +30,11 @@ public class InnocentNimbusItem extends ArmorItem implements GeoItem {
 
 	private final AnimatableInstanceCache cache = AzureLibUtil.createInstanceCache(this);
 	private final Supplier<Object> renderProvider = GeoItem.makeRenderer(this);
+
+
+
+
+
 
 	public InnocentNimbusItem(ArmorMaterial armorMaterial, ArmorSlot type, Settings properties) {
 		super(armorMaterial, type, properties);
@@ -53,10 +46,19 @@ public class InnocentNimbusItem extends ArmorItem implements GeoItem {
 	public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
 		if(!world.isClient()) {
 			if(entity instanceof PlayerEntity player && hasFullSuitOfArmorOn(player)) {
-				player.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 25,99, false, false, false));
-				player.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 25,99, false, false, false));
+				if(hasCorrectArmorOn(ModArmorMaterials.INNOCENCE, player)) {
+					addStatusEffectForMaterial(player, ModArmorMaterials.INNOCENCE, new StatusEffectInstance(StatusEffects.RESISTANCE,
+						25, 99, false, false, false));
+					addStatusEffectForMaterial(player, ModArmorMaterials.INNOCENCE, new StatusEffectInstance(StatusEffects.REGENERATION,
+						25, 99, false, false, false));
+					addStatusEffectForMaterial(player, ModArmorMaterials.INNOCENCE, new StatusEffectInstance(StatusEffects.WEAKNESS,
+						25, 99, false, false, false));
+					addStatusEffectForMaterial(player, ModArmorMaterials.INNOCENCE, new StatusEffectInstance(StatusEffects.SATURATION,
+						25, 99, false, false, false));
 
+					//i think would be better to change to a custom effect, that will apply all above at once, but display as 1
 
+				}
 			}
 		}
 
@@ -65,19 +67,51 @@ public class InnocentNimbusItem extends ArmorItem implements GeoItem {
 
 
 
-	private boolean hasFullSuitOfArmorOn(PlayerEntity player) {
+	private void addStatusEffectForMaterial(@NotNull PlayerEntity player, ArmorMaterial ArmorMaterial, @NotNull StatusEffectInstance StatusEffect) {
+
+		if(hasCorrectArmorOn(ArmorMaterial, player)) {
+			player.addStatusEffect(new StatusEffectInstance(StatusEffect));
+		}
+	}
+
+	private boolean hasFullSuitOfArmorOn(@NotNull PlayerEntity player) {
 
 		ItemStack helmet = player.getInventory().getArmorStack(3);
 
 		return !helmet.isEmpty();
 	}
 
+	private boolean hasCorrectArmorOn(ArmorMaterial material, @NotNull PlayerEntity player) {
 
 
+
+		ArmorItem helmet = ((ArmorItem)player.getInventory().getArmorStack(3).getItem());
+
+		return helmet.getMaterial() == material;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
 	//azurelib part
 
 	@Override
-	public void createRenderer(Consumer<Object> consumer) {
+	public void createRenderer(@NotNull Consumer<Object> consumer) {
 		consumer.accept(new RenderProvider() {
 			private GeoArmorRenderer<?> renderer;
 
@@ -102,7 +136,7 @@ public class InnocentNimbusItem extends ArmorItem implements GeoItem {
 
 	// Let's add our animation controller
 	@Override
-	public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+	public void registerControllers(AnimatableManager.@NotNull ControllerRegistrar controllers) {
 		controllers.add(new AnimationController<>(this, "controllerName", 0, event ->
 		{
 			return event.setAndContinue(RawAnimation.begin().thenLoop("idle"));
