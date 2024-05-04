e319be36c24157d3250da5832c39ad7b33bf9d3b
Nikit <astraimer.ru@gmail.com>
2024-05-05 00:44:41 +0200

    sorry stone item has been added. Needs re-texturing

--- src/main/java/nikita/uniquescythe/items/custom/SorryStoneItem.java
+++ src/main/java/nikita/uniquescythe/items/custom/SorryStoneItem.java
@@ -17,9 +17,14 @@ public class SorryStoneItem extends Item {
 	@Override
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand){
 
-		GuiltyLevelSystem.removeGuiltyLevel(user, user.getDisplayName().getString(), 5, 5);
-		user.kill();
+		if (!world.isClient){//if the problem is in casting unexpected client side classes to server side ones, the problem is that i haven't split them on 2 different cases: client and server
+			GuiltyLevelSystem.removeGuiltyLevel((ServerPlayerEntity) user, user.getDisplayName().getString(), 5, 5);
+			ItemStack stack = user.getStackInHand(hand);
+			stack.decrement(1);
+			user.kill();
 
-		return TypedActionResult.pass(user.getStackInHand(hand));
+			return TypedActionResult.pass(user.getStackInHand(hand));
+		}
+		else return TypedActionResult.fail(user.getStackInHand(hand));
 	}
 }
