85078b1eff3de70a051349d1a997a6f96c6d1e04
Nikit <astraimer.ru@gmail.com>
2024-05-15 10:15:13 +0200

    terrablender added

---
+++ src/main/java/nikita/uniquescythe/world/UniqueScytheTerrablender.java
@@ -0,0 +1,12 @@
+package nikita.uniquescythe.world;
+
+import terrablender.api.TerraBlenderApi;
+
+public class UniqueScytheTerrablender implements TerraBlenderApi {
+
+	@Override
+	public void onTerraBlenderInitialized()
+	{
+		// Your code here.
+	}
+}
