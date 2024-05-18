c0dc626717fe6159e632d93a0c7b5c9213d2dd67
Nikita <astraimer.ru@gmail.com>
2024-05-18 23:38:05 +0200

    🥸

--- src/main/java/nikita/uniquescythe/utility/ItemsPosing.java
+++ src/main/java/nikita/uniquescythe/utility/ItemsPosing.java
@@ -2,6 +2,7 @@ package nikita.uniquescythe.utility;
 
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.entity.LivingEntity;
+import nikita.uniquescythe.items.ModItems;
 import nikita.uniquescythe.items.custom.GunItem;
 import nikita.uniquescythe.items.custom.JusticeRevolverItem;
 import org.quiltmc.loader.api.minecraft.ClientOnly;
@@ -15,12 +16,15 @@ public class ItemsPosing {
 			ModelPart modelPart = rightArmed ? holdingArm : otherArm;
 			ModelPart modelPart2 = rightArmed ? otherArm : holdingArm;
 
-			modelPart2.yaw = (rightArmed ? 0.5F : -0.5F) + head.yaw;
+
+
+			modelPart2.yaw = (rightArmed ? 1F : -1F) + head.yaw;
 
 			modelPart2.pitch = ((float) (-Math.PI / 2) ) + head.pitch;
 
+
 			//inn or away of the players body.
-			modelPart.yaw =head.yaw + (rightArmed ? -0.5F : 0.5F);
+			modelPart.yaw =head.yaw + (rightArmed ? -0.1F : 0.1F);
 
 			//this one seems to respond for a horizontal axis(is arm towards ceiling or floor)
 			modelPart.pitch = (float) (-Math.PI / 2) + head.pitch;
