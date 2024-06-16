aeec236ca0f5000efaf1021e71b0a9f681551f68
Nikita <astraimer.ru@gmail.com>
2024-06-16 22:26:30 +0200

    work on miner block fc

--- src/main/java/nikita/uniquescythe/blocks/custom/MinerBlock.java
+++ src/main/java/nikita/uniquescythe/blocks/custom/MinerBlock.java
@@ -31,7 +31,7 @@ public class MinerBlock extends HorizontallyDirectionalBlock{
 	public void scanBlocksBelow(BlockState state, ServerWorld world, BlockPos pos) {
 		if (!world.isClient) {
 			boolean isCharged = world.isReceivingRedstonePower(pos);
-			int blocksFound = 0;
+
 			boolean shouldDropIron = false;
 			boolean shouldDropRedstone = false;
 			boolean shouldDropDiamonds = false;
@@ -47,107 +47,104 @@ public class MinerBlock extends HorizontallyDirectionalBlock{
 
 			if (isCharged) {
 				for (int i = 1; i <= 50; i++) {
+					BlockState checkState = null;
 					BlockPos checkPos = pos.down(i);
-					BlockState checkState = world.getBlockState(checkPos);
-
-					// Check for a specific block, for example, an iron ore block
-					if (checkState.isOf(Blocks.IRON_ORE)
-						|| checkState.isOf(Blocks.DEEPSLATE_IRON_ORE)) {
-						shouldDropIron = true;
-
-					} else shouldDropIron = false;
-					if (checkState.isOf(Blocks.GOLD_ORE)
-						|| checkState.isOf(Blocks.DEEPSLATE_GOLD_ORE)) {
-						shouldDropGold= true;
-
-					}
-					if (checkState.isOf(Blocks.REDSTONE_ORE)
-						|| checkState.isOf(Blocks.DEEPSLATE_REDSTONE_ORE)) {
-						shouldDropRedstone= true;
-
-					}
-					if (checkState.isOf(Blocks.DIAMOND_ORE)
-						|| checkState.isOf(Blocks.DEEPSLATE_DIAMOND_ORE)) {
-						shouldDropDiamonds= true;
-
-					}
-					if (checkState.isOf(Blocks.AMETHYST_BLOCK)
-						|| checkState.isOf(Blocks.AMETHYST_CLUSTER)) {
-						shouldDropAmethyst= true;
-
-					}
-					if (checkState.isOf(Blocks.EMERALD_ORE)
-						|| checkState.isOf(Blocks.DEEPSLATE_EMERALD_ORE)) {
-						shouldDropEmeralds= true;
-
-					}
-					if (checkState.isOf(Blocks.COPPER_ORE)
-						|| checkState.isOf(Blocks.DEEPSLATE_COPPER_ORE)) {
-						shouldDropCopper= true;
-
-					}
-					if (checkState.isOf(Blocks.LAPIS_ORE)
-						|| checkState.isOf(Blocks.DEEPSLATE_LAPIS_ORE)) {
-						shouldDropLapis= true;
-
-					}
+					checkState = world.getBlockState(checkPos);
 					if (!checkState.isAir() && checkState.isSolidBlock(world, checkPos)) {
 
-						blocksFound++;
 
-					}
-					if (checkState.isAir()) {
-						break; // Stop scanning if an air block is found
-					}
+						// Check for a specific block, for example, an iron ore block
+						if (checkState.isOf(Blocks.IRON_ORE)
+							|| checkState.isOf(Blocks.DEEPSLATE_IRON_ORE)) {
+							shouldDropIron = true;
+
+						} else shouldDropIron = false;
+						if (checkState.isOf(Blocks.GOLD_ORE)
+							|| checkState.isOf(Blocks.DEEPSLATE_GOLD_ORE)) {
+							shouldDropGold= true;
+
+						}
+						if (checkState.isOf(Blocks.REDSTONE_ORE)
+							|| checkState.isOf(Blocks.DEEPSLATE_REDSTONE_ORE)) {
+							shouldDropRedstone= true;
+
+						}
+						if (checkState.isOf(Blocks.DIAMOND_ORE)
+							|| checkState.isOf(Blocks.DEEPSLATE_DIAMOND_ORE)) {
+							shouldDropDiamonds= true;
+
+						}
+						if (checkState.isOf(Blocks.AMETHYST_BLOCK)
+							|| checkState.isOf(Blocks.AMETHYST_CLUSTER)) {
+							shouldDropAmethyst= true;
+
+						}
+						if (checkState.isOf(Blocks.EMERALD_ORE)
+							|| checkState.isOf(Blocks.DEEPSLATE_EMERALD_ORE)) {
+							shouldDropEmeralds= true;
+
+						}
+						if (checkState.isOf(Blocks.COPPER_ORE)
+							|| checkState.isOf(Blocks.DEEPSLATE_COPPER_ORE)) {
+							shouldDropCopper= true;
+
+						}
+						if (checkState.isOf(Blocks.LAPIS_ORE)
+							|| checkState.isOf(Blocks.DEEPSLATE_LAPIS_ORE)) {
+							shouldDropLapis= true;
+
+						}
+
+						// Drop the item
+						ItemEntity itemEntity;
+						List<ItemStack> possibleItems = new ArrayList<>();
+
+						if (shouldDropDiamonds) {
+							possibleItems.add(new ItemStack(Items.DIAMOND));
+						}
+
+						if (shouldDropIron) {
+							possibleItems.add(new ItemStack(Items.RAW_IRON));
+						}
+
+						if (shouldDropGold) {
+							possibleItems.add(new ItemStack(Items.RAW_GOLD));
+						}
+
+						if (shouldDropRedstone) {
+							possibleItems.add(new ItemStack(Items.REDSTONE));
+						}
+
+						if (shouldDropAmethyst) {
+							possibleItems.add(new ItemStack(Items.AMETHYST_SHARD));
+						}
+
+						if (shouldDropEmeralds) {
+							possibleItems.add(new ItemStack(Items.EMERALD));
+						}
+
+						if (shouldDropCopper) {
+							possibleItems.add(new ItemStack(Items.RAW_COPPER));
+						}
+
+						if (shouldDropLapis) {
+							possibleItems.add(new ItemStack(Items.LAPIS_LAZULI));
+						}
+
+
+						if (!possibleItems.isEmpty()) {
+							Random random = new Random();
+							ItemStack selectedItem = possibleItems.get(random.nextInt(possibleItems.size()));
+							itemEntity = new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), selectedItem);
+							world.spawnEntity(itemEntity);
+						}
+
+					} else break;
 				}
 			}
 
 			// If the block is charged and the specified block was found
-			if (isCharged && blocksFound > 0) {
-				// Drop the item
-				ItemEntity itemEntity = null;
-				List<ItemStack> possibleItems = new ArrayList<>();
 
-				if (shouldDropDiamonds) {
-					possibleItems.add(new ItemStack(Items.DIAMOND));
-				}
-
-				if (shouldDropIron) {
-					possibleItems.add(new ItemStack(Items.RAW_IRON));
-				}
-
-				if (shouldDropGold) {
-					possibleItems.add(new ItemStack(Items.RAW_GOLD));
-				}
-
-				if (shouldDropRedstone) {
-					possibleItems.add(new ItemStack(Items.REDSTONE));
-				}
-
-				if (shouldDropAmethyst) {
-					possibleItems.add(new ItemStack(Items.AMETHYST_SHARD));
-				}
-
-				if (shouldDropEmeralds) {
-					possibleItems.add(new ItemStack(Items.EMERALD));
-				}
-
-				if (shouldDropCopper) {
-					possibleItems.add(new ItemStack(Items.RAW_COPPER));
-				}
-
-				if (shouldDropLapis) {
-					possibleItems.add(new ItemStack(Items.LAPIS_LAZULI));
-				}
-
-
-				if (!possibleItems.isEmpty()) {
-					Random random = new Random();
-					ItemStack selectedItem = possibleItems.get(random.nextInt(possibleItems.size()));
-					itemEntity = new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), selectedItem);
-					world.spawnEntity(itemEntity);
-				}
-			}
 		}
 	}
 
