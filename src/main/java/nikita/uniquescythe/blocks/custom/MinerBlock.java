a7229c0a40bcf6d9fae1c55d95da3f9dc152fb71
Nikita <astraimer.ru@gmail.com>
2024-06-16 22:16:50 +0200

    work on miner block fc

--- src/main/java/nikita/uniquescythe/blocks/custom/MinerBlock.java
+++ src/main/java/nikita/uniquescythe/blocks/custom/MinerBlock.java
@@ -1,25 +1,34 @@
 package nikita.uniquescythe.blocks.custom;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class MinerBlock extends HorizontallyDirectionalBlock{
+	public static final BooleanProperty POWERED = Properties.POWERED;
+	public static final BooleanProperty OPENED = BooleanProperty.of("opened");
+
+
 	public MinerBlock(Settings settings) {
 		super(settings);
+		setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
 	}
 
 	// Method to scan blocks underneath
-	public void scanBlocksBelow(World world, BlockPos pos) {
+	public void scanBlocksBelow(BlockState state, ServerWorld world, BlockPos pos) {
 		if (!world.isClient) {
 			boolean isCharged = world.isReceivingRedstonePower(pos);
 			int blocksFound = 0;
@@ -28,48 +37,69 @@ public class MinerBlock extends HorizontallyDirectionalBlock{
 			boolean shouldDropDiamonds = false;
 			boolean shouldDropGold = false;
 			boolean shouldDropAmethyst = false;
+			boolean shouldDropEmeralds = false;
+			boolean shouldDropCopper = false;
+			boolean shouldDropLapis = false;
 
 
+			world.setBlockState(pos, (BlockState)state.with(POWERED, isCharged), Block.NOTIFY_ALL);
 
-			for (int i = 1; i <= 50; i++) {
-				BlockPos checkPos = pos.down(i);
-				BlockState checkState = world.getBlockState(checkPos);
 
-				// Check for a specific block, for example, an iron ore block
-				if (checkState.isOf(Blocks.IRON_ORE)) {
-					shouldDropIron = true;
+			if (isCharged) {
+				for (int i = 1; i <= 50; i++) {
+					BlockPos checkPos = pos.down(i);
+					BlockState checkState = world.getBlockState(checkPos);
 
-				} else shouldDropIron = false;
-				if (checkState.isOf(Blocks.GOLD_ORE)) {
-					shouldDropGold= true;
+					// Check for a specific block, for example, an iron ore block
+					if (checkState.isOf(Blocks.IRON_ORE)
+						|| checkState.isOf(Blocks.DEEPSLATE_IRON_ORE)) {
+						shouldDropIron = true;
 
+					} else shouldDropIron = false;
+					if (checkState.isOf(Blocks.GOLD_ORE)
+						|| checkState.isOf(Blocks.DEEPSLATE_GOLD_ORE)) {
+						shouldDropGold= true;
+
+					}
+					if (checkState.isOf(Blocks.REDSTONE_ORE)
+						|| checkState.isOf(Blocks.DEEPSLATE_REDSTONE_ORE)) {
+						shouldDropRedstone= true;
+
+					}
+					if (checkState.isOf(Blocks.DIAMOND_ORE)
+						|| checkState.isOf(Blocks.DEEPSLATE_DIAMOND_ORE)) {
+						shouldDropDiamonds= true;
+
+					}
+					if (checkState.isOf(Blocks.AMETHYST_BLOCK)
+						|| checkState.isOf(Blocks.AMETHYST_CLUSTER)) {
+						shouldDropAmethyst= true;
+
+					}
+					if (checkState.isOf(Blocks.EMERALD_ORE)
+						|| checkState.isOf(Blocks.DEEPSLATE_EMERALD_ORE)) {
+						shouldDropEmeralds= true;
+
+					}
+					if (checkState.isOf(Blocks.COPPER_ORE)
+						|| checkState.isOf(Blocks.DEEPSLATE_COPPER_ORE)) {
+						shouldDropCopper= true;
+
+					}
+					if (checkState.isOf(Blocks.LAPIS_ORE)
+						|| checkState.isOf(Blocks.DEEPSLATE_LAPIS_ORE)) {
+						shouldDropLapis= true;
+
+					}
+					if (!checkState.isAir() && checkState.isSolidBlock(world, checkPos)) {
+
+						blocksFound++;
+
+					}
+					if (checkState.isAir()) {
+						break; // Stop scanning if an air block is found
+					}
 				}
-				if (checkState.isOf(Blocks.REDSTONE_ORE)) {
-					shouldDropRedstone= true;
-
-				}
-				if (checkState.isOf(Blocks.DIAMOND_ORE)) {
-					shouldDropDiamonds= true;
-
-				}
-				if (checkState.isOf(Blocks.AMETHYST_BLOCK)
-				|| checkState.isOf(Blocks.AMETHYST_CLUSTER)) {
-					shouldDropAmethyst= true;
-
-				}
-
-
-				if (!checkState.isAir() && checkState.isSolidBlock(world, checkPos)) {
-
-					blocksFound++;
-
-				}
-
-				if (checkState.isAir()) {
-					break; // Stop scanning if an air block is found
-				}
-
-
 			}
 
 			// If the block is charged and the specified block was found
@@ -83,11 +113,11 @@ public class MinerBlock extends HorizontallyDirectionalBlock{
 				}
 
 				if (shouldDropIron) {
-					possibleItems.add(new ItemStack(Items.IRON_INGOT));
+					possibleItems.add(new ItemStack(Items.RAW_IRON));
 				}
 
 				if (shouldDropGold) {
-					possibleItems.add(new ItemStack(Items.GOLD_INGOT));
+					possibleItems.add(new ItemStack(Items.RAW_GOLD));
 				}
 
 				if (shouldDropRedstone) {
@@ -98,22 +128,34 @@ public class MinerBlock extends HorizontallyDirectionalBlock{
 					possibleItems.add(new ItemStack(Items.AMETHYST_SHARD));
 				}
 
+				if (shouldDropEmeralds) {
+					possibleItems.add(new ItemStack(Items.EMERALD));
+				}
+
+				if (shouldDropCopper) {
+					possibleItems.add(new ItemStack(Items.RAW_COPPER));
+				}
+
+				if (shouldDropLapis) {
+					possibleItems.add(new ItemStack(Items.LAPIS_LAZULI));
+				}
+
+
 				if (!possibleItems.isEmpty()) {
 					Random random = new Random();
 					ItemStack selectedItem = possibleItems.get(random.nextInt(possibleItems.size()));
 					itemEntity = new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), selectedItem);
 					world.spawnEntity(itemEntity);
 				}
-
-				// Set the block on cooldown (example: 5 minutes)
-				// You'll need to handle the cooldown logic separately
 			}
-
-			// If there are other instances of the block
-			// You'll need to scan the surrounding area and count instances of MinerBlock
-			// This can be done similarly to the scanning logic above
 		}
 	}
 
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		super.appendProperties(builder);
+		// Add the HORIZONTAL_FACING property
+		builder.add(POWERED, OPENED);
+	}
 
 }
