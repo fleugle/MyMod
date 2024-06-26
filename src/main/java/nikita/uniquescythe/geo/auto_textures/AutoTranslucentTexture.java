2396d3c6f790845add2938b71a94cca8b4797e6c
Nikita <astraimer.ru@gmail.com>
2024-06-26 12:01:26 +0200

    3

--- src/main/java/nikita/uniquescythe/geo/auto_textures/AutoTranslucentTexture.java
+++ src/main/java/nikita/uniquescythe/geo/auto_textures/AutoTranslucentTexture.java
@@ -1,16 +1,27 @@
 package nikita.uniquescythe.geo.auto_textures;
 
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderCall;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.texture.NativeImage;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormats;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 import java.util.function.BiFunction;
+import java.util.function.Function;
+
 import mod.azure.azurelib.cache.texture.GeoAbstractTexture;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderPhase;
+import net.minecraft.client.resource.metadata.TextureResourceMetadata;
+import net.minecraft.client.texture.AbstractTexture;
+import net.minecraft.client.texture.NativeImageBackedTexture;
+import net.minecraft.resource.Resource;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
@@ -22,36 +33,70 @@ import static net.minecraft.client.render.RenderPhase.*;
 
 
 public class AutoTranslucentTexture extends GeoAbstractTexture {
+	protected final Identifier textureBase;
+
+	private static final RenderPhase.Shader SHADER_STATE = new RenderPhase.Shader(GameRenderer::getRenderTypeEntityTranslucentShader);
+	private static final RenderPhase.Transparency TRANSPARENCY_STATE = new RenderPhase.Transparency("translucent_transparency", () -> {
+		RenderSystem.enableBlend();
+		RenderSystem.disableCull();
+		RenderSystem.defaultBlendFunc();
+		//RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+	}, () -> {
+		RenderSystem.enableCull();
+		RenderSystem.disableBlend();
+		RenderSystem.defaultBlendFunc();
+	});
+
+	private static final Function<Identifier, RenderLayer> ENTITY_TRANSLUCENT = Util.memoize((texture) -> {
+		RenderPhase.Texture textureState = new RenderPhase.Texture(texture, false, false);
+		return RenderLayer.of("geo_translucent_layer",
+			VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL,
+			VertexFormat.DrawMode.QUADS,
+			256,
+			false,
+			true,
+			RenderLayer.MultiPhaseParameters.builder()
+				.shader(SHADER_STATE)
+				.texture(textureState)
+				.transparency(TRANSPARENCY_STATE)
+				.build(false));
+	});
 
 
-
-
-	private static final BiFunction<Identifier, Boolean, RenderLayer> ENTITY_TRANSLUCENT = Util.memoize(
-		(BiFunction)((texture, affectsOutline) -> {
-			RenderLayer.MultiPhaseParameters multiPhaseParameters = RenderLayer.MultiPhaseParameters.builder()
-				.shader(ENTITY_TRANSLUCENT_SHADER)
-				.texture(new RenderPhase.Texture((Identifier) texture, false, false))
-				.transparency(TRANSLUCENT_TRANSPARENCY)
-				.cull(DISABLE_CULLING)
-				.lightmap(ENABLE_LIGHTMAP)
-				.overlay(ENABLE_OVERLAY_COLOR)
-				.build((Boolean) affectsOutline);
-			return RenderLayer.of(
-				"entity_translucent", VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL, VertexFormat.DrawMode.QUADS, 256, true, true, multiPhaseParameters
-			);
-		})
-	);
-
-
-
-
-
-	public static RenderLayer getEntityTranslucent(Identifier texture, boolean affectsOutline) {
-		return ENTITY_TRANSLUCENT.apply(texture, affectsOutline);
+	public AutoTranslucentTexture(Identifier originalLocation) {
+		this.textureBase = originalLocation;
 	}
 
+
+
+
+
+
 	@Override
-	protected @Nullable RenderCall loadTexture(ResourceManager resourceManager, MinecraftClient minecraftClient) throws IOException {
-		return null;
+	protected @Nullable RenderCall loadTexture(ResourceManager resourceManager, MinecraftClient mc) throws IOException {
+		AbstractTexture originalTexture;
+		try {
+			originalTexture = (AbstractTexture) mc.submit(() -> mc.getTextureManager().getTexture(this.textureBase)).get();
+		} catch (ExecutionException | InterruptedException e) {
+			throw new IOException("Failed to load original texture: " + this.textureBase, e);
+		}
+
+		Resource textureBaseResource = (Resource) resourceManager.getResource(this.textureBase).get();
+		NativeImage baseImage;
+		if (originalTexture instanceof NativeImageBackedTexture dynamicTexture) {
+			baseImage = dynamicTexture.getImage();
+		} else {
+			baseImage = NativeImage.read(textureBaseResource.open());
+		}
+
+		Optional<TextureResourceMetadata> textureBaseMeta = textureBaseResource.getMetadata().readMetadata(TextureResourceMetadata.READER);
+		boolean blur = textureBaseMeta.isPresent() && ((TextureResourceMetadata) textureBaseMeta.get()).shouldBlur();
+		boolean clamp = textureBaseMeta.isPresent() && ((TextureResourceMetadata) textureBaseMeta.get()).shouldClamp();
+
+		return () -> uploadSimple(originalTexture.getGlId(), baseImage, blur, clamp);
+	}
+
+	public static RenderLayer getEntityTranslucent(Identifier texture) {
+		return ENTITY_TRANSLUCENT.apply(texture);
 	}
 }
--- src/main/java/nikita/uniquescythe/geo/render_layers/TranslucentLayer.java
+++ src/main/java/nikita/uniquescythe/geo/render_layers/TranslucentLayer.java
@@ -2,7 +2,6 @@ package nikita.uniquescythe.geo.render_layers;
 
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import mod.azure.azurelib.cache.object.BakedGeoModel;
-import mod.azure.azurelib.cache.texture.AutoGlowingTexture;
 import mod.azure.azurelib.renderer.GeoRenderer;
 import mod.azure.azurelib.renderer.layer.GeoRenderLayer;
 import net.minecraft.client.render.OverlayTexture;
@@ -14,8 +13,6 @@ import nikita.uniquescythe.entities.custom.WindChargeProjectileEntity;
 import nikita.uniquescythe.geo.auto_textures.AutoTranslucentTexture;
 
 public class TranslucentLayer<T extends WindChargeProjectileEntity> extends GeoRenderLayer<T> {
-	private static final Identifier TEXTURE = new Identifier("uniquescythe","textures/entity/wind_charge.png");
-	private static final RenderLayer LAYER = RenderLayer.getEntityTranslucent(TEXTURE);
 
 	public TranslucentLayer(GeoRenderer<T> renderer) {
 		super(renderer);
@@ -23,13 +20,13 @@ public class TranslucentLayer<T extends WindChargeProjectileEntity> extends GeoR
 
 
 	protected RenderLayer getRenderType(T animatable) {
-		return AutoTranslucentTexture.getEntityTranslucent(this.getTextureResource(animatable),false);
+		return AutoTranslucentTexture.getEntityTranslucent(this.getTextureResource(animatable));
 	}
 
 	@Override
 	public void render(MatrixStack poseStack, T animatable, BakedGeoModel bakedModel, RenderLayer renderType, VertexConsumerProvider bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
 
 		RenderLayer translucentRenderLayer = this.getRenderType(animatable);
-		this.getRenderer().reRender(bakedModel, poseStack, bufferSource, animatable, translucentRenderLayer, bufferSource.getBuffer(translucentRenderLayer), partialTick, packedLight, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 0.6F);
+		this.getRenderer().reRender(bakedModel, poseStack, bufferSource, animatable, translucentRenderLayer, bufferSource.getBuffer(translucentRenderLayer), partialTick, packedLight, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1F);
 	}
 }
